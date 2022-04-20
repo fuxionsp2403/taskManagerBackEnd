501a6cbad796191bbbf45dc2ca0cd50fc01d15f3
NevermindG <gonzaloorlando2001@gmail.com>
2022-04-19 21:57:01 -0500

    initial commit

---
+++ src/main/java/com/max/taskmanagermax_api/TaskManagerMaxApiApplication.java
@@ -0,0 +1,13 @@
+package com.max.taskmanagermax_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerMaxApiApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerMaxApiApplication.class, args);
+    }
+    
+}
