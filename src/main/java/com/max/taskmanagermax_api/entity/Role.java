821d1383c310e750865b411e461a2588ab5b0612
NevermindG <gonzaloorlando2001@gmail.com>
2022-04-27 16:52:02 -0500

    created user and role entities

---
+++ src/main/java/com/max/taskmanagermax_api/entity/Role.java
@@ -0,0 +1,24 @@
+package com.max.taskmanagermax_api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table (name = "roles")
+public class Role {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(length = 60)
+    private String name;
+    
+}
