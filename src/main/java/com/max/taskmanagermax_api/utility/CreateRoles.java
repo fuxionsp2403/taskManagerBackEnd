46ae18f324ac1dbe894b3f6b4d2f588ae24d12a3
NevermindG <gonzaloorlando2001@gmail.com>
2022-05-16 19:16:53 -0500

    validating roles

--- src/main/java/com/max/taskmanagermax_api/utility/CreateRoles.java
+++ src/main/java/com/max/taskmanagermax_api/utility/CreateRoles.java
@@ -3,24 +3,31 @@ package com.max.taskmanagermax_api.utility;
 import com.max.taskmanagermax_api.entity.Role;
 import com.max.taskmanagermax_api.enums.RoleName;
 import com.max.taskmanagermax_api.service.RoleService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class CreateRoles implements CommandLineRunner {
-
+    
     private final RoleService roleService;
-
+    
     public CreateRoles(RoleService roleService) {
         this.roleService = roleService;
     }
-
+    
     @Override
     public void run(String... args) throws Exception {
-        Role adminRole = new Role (RoleName.ROLE_ADMIN);
-        Role userRole= new Role (RoleName.ROLE_USER);
-        roleService.save(adminRole);
-        roleService.save(userRole);
+        if (roleService.getByRoleName(RoleName.ROLE_ADMIN).isEmpty() && roleService.getByRoleName(RoleName.ROLE_USER).isEmpty()) {
+            Role adminRole = new Role(RoleName.ROLE_ADMIN);
+            Role userRole = new Role(RoleName.ROLE_USER);
+            roleService.save(adminRole);
+            roleService.save(userRole);
+        } else {
+            log.error("Roles already exist");
+            
+        }
     }
-
+    
 }
